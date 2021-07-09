5ad8091425dce258d4605cc107b82ec127d95e66
harriszhang <harriszhang@live.cn>
2021-07-10 00:55:37 +0800

    update

--- 0 - Build-Publish-Move.bat
+++ 0 - Build-Publish-Move.bat
@@ -5,5 +5,6 @@ dotnet restore
 dotnet build --no-restore -c Release
 
 move /Y Panosen.CodeDom.CSharp\bin\Release\Panosen.CodeDom.CSharp.*.nupkg D:\LocalSavoryNuget\
+move /Y Panosen.CodeDom.CSharp.Engine\bin\Release\Panosen.CodeDom.CSharp.Engine.*.nupkg D:\LocalSavoryNuget\
 
 pause
