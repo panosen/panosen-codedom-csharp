f3cbb68fe01772d14a334b6b9852d84125eddbc1
harriszhang <harriszhang@live.cn>
2022-06-07 10:33:20 +0800

    add ut

--- Panosen.CodeDom.CSharp.Engine.MSTest/UT_CodeClass_5.cs
+++ Panosen.CodeDom.CSharp.Engine.MSTest/UT_CodeClass_5.cs
@@ -19,6 +19,12 @@ public class Student
 
     [Newtonsoft.Json.JsonProperty(""age"", NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
     public int Age { get; set; }
+
+    [Newtonsoft.Json.JsonProperty(""name"")]
+    public string Name { get; set; }
+
+    [Newtonsoft.Json.JsonProperty(key = ""123"")]
+    public string Remark { get; set; }
 }
 ";
         }
@@ -30,11 +36,18 @@ public class Student
             codeClass.Summary = "学生";
             codeClass.AccessModifiers = AccessModifiers.Public;
 
-            var codeProperty = codeClass.AddProperty("int", "Age");
-
-            codeProperty.AddAttribute("Newtonsoft.Json.JsonProperty")
+            codeClass.AddProperty("int", "Age")
+                .AddAttribute("Newtonsoft.Json.JsonProperty")
                 .AddStringParam("age")
                 .AddPlainParam("NullValueHandling", "Newtonsoft.Json.NullValueHandling.Ignore");
+
+            codeClass.AddProperty("string", "Name")
+                .AddAttribute("Newtonsoft.Json.JsonProperty")
+                .AddStringParam("name");
+
+            codeClass.AddProperty("string", "Remark")
+                .AddAttribute("Newtonsoft.Json.JsonProperty")
+                .AddStringParam("key", "123");
 
             return codeClass;
         }
