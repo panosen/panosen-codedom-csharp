ac554930cf095d9f695ac6378333eb1bf0c62b3a
hc_zhang <hc_zhang@trip.com>
2022-11-10 10:42:32 +0800

    1.4.3 update engine

--- Panosen.CodeDom.CSharp.Engine/StepBuilders/CSharpCodeEngine_StepBuilderOrCollection.cs
+++ Panosen.CodeDom.CSharp.Engine/StepBuilders/CSharpCodeEngine_StepBuilderOrCollection.cs
@@ -11,9 +11,6 @@ namespace Panosen.CodeDom.CSharp.Engine
         /// <summary>
         /// 生成方法步骤
         /// </summary>
-        /// <param name="stepBuilder"></param>
-        /// <param name="codeWriter"></param>
-        /// <param name="options"></param>
         public void GenerateStepBuilderOrCollection(StepBuilderOrCollection stepBuilder, CodeWriter codeWriter, GenerateOptions options = null)
         {
             if (stepBuilder == null) { return; }
@@ -28,61 +25,83 @@ namespace Panosen.CodeDom.CSharp.Engine
             if (stepBuilder is StatementStepBuilder)
             {
                 GenerateStatementStepBuilder(stepBuilder as StatementStepBuilder, codeWriter, options);
+                return;
             }
 
             if (stepBuilder is UsingStepBuilder)
             {
                 GenerateUsingStepBuilder(stepBuilder as UsingStepBuilder, codeWriter, options);
+                return;
             }
 
             if (stepBuilder is IfStepBuilder)
             {
                 GenerateIfStepBuilder(stepBuilder as IfStepBuilder, codeWriter, options);
+                return;
             }
 
             if (stepBuilder is WhileStepBuilder)
             {
                 GenerateWhileStepBuilder(stepBuilder as WhileStepBuilder, codeWriter, options);
+                return;
             }
 
             if (stepBuilder is TryStepBuilder)
             {
                 GenerateTryStepBuilder(stepBuilder as TryStepBuilder, codeWriter, options);
+                return;
             }
 
             if (stepBuilder is SwitchStepBuilder)
             {
                 GenerateSwitchStepBuilder(stepBuilder as SwitchStepBuilder, codeWriter, options);
+                return;
             }
 
             if (stepBuilder is ForeachStepBuilder)
             {
                 GenerateForeachStepBuilder(stepBuilder as ForeachStepBuilder, codeWriter, options);
+                return;
             }
 
             if (stepBuilder is ForStepBuilder)
             {
                 GenerateForStepBuilder(stepBuilder as ForStepBuilder, codeWriter, options);
+                return;
             }
 
             if (stepBuilder is BlockStepBuilder)
             {
                 GenerateBlockStepBuilder(stepBuilder as BlockStepBuilder, codeWriter, options);
+                return;
             }
 
             if (stepBuilder is PushIndentStepBuilder)
             {
                 GeneratePushIndentStepBuilder(stepBuilder as PushIndentStepBuilder, codeWriter, options);
+                return;
             }
 
             if (stepBuilder is StatementChainStepBuilder)
             {
                 GenerateStatementChainStepBuilder(stepBuilder as StatementChainStepBuilder, codeWriter, options);
+                return;
             }
 
             if (stepBuilder is AssignmentStepBuilder)
             {
                 GenerateAssignmentStepBuilder(stepBuilder as AssignmentStepBuilder, codeWriter, options);
+                return;
+            }
+
+            if (stepBuilder is StepBuilderCollection)
+            {
+                var stepBuilderCollection = stepBuilder as StepBuilderCollection;
+                foreach (var item in stepBuilderCollection.StepBuilders)
+                {
+                    GenerateStepBuilderOrCollection(item, codeWriter, options);
+                }
+                return;
             }
         }
     }
